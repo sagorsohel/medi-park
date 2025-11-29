bf9049635e47d84d2e2829953109cba76af7c546
sagorsohel <sohel19sagor@gmail.com>
2025-11-30 00:00:26 +0600

    Update ThemeProvider default theme to 'light' for improved user experience.

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { store } from './store'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
         <RouterProvider router={router} />
       </ThemeProvider>
     </Provider>
